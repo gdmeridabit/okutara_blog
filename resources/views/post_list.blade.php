a2de2299fc5949aef2ca777b9fd48ff6d18fb28c
Micaela <eladelmonte@gmail.com>
2019-09-03 22:32:17 +0900

    UPDATED post list ui

--- resources/views/post_list.blade.php
+++ resources/views/post_list.blade.php
@@ -2,31 +2,33 @@
 
 @section('content')
 <div class="container">
-  <div class="text-center mt-5 mb-4">
-    <h2 class="font-weight-bold">{{$category[0]->name}}</h2>
-  </div>
-  <div class="row">
-    @foreach($category as $data)
-      @foreach($data->posts as $post)
-      <div class="col-4">
-        <a href="/post/{{ $post->id }}" class="card-link">
-          <div class="card ml-2 mb-2 list">
-            <div style="min-height: 200px; width: 100%">
-              <img src="{{asset('storage/files/'. $post->filename)}}" class="img-fluid" />
-            </div>
-            <div class="card-body">
-              <h4 class="card-title text-dark h4 font-weight-bold">{{ $post->title }}</h4>
-              <p class="text-dark small">{{ $post->user->name }} | {{ $post->created_at }}</p>
-              <p class="text-dark text-truncate">{{ $post->description }}</p>
-              <div class="text-dark text-right">
-                <i class="fas fa-thumbs-up ml-2 mr-1"></i> {{ count($post->likes) }}
+  <div class="categories-content">
+    <div class="text-center mt-5 mb-4">
+      <h2 class="font-weight-bold">{{$category[0]->name}}</h2>
+    </div>
+    <div class="row">
+      @foreach($category as $data)
+        @foreach($data->posts as $post)
+        <div class="col-4">
+          <a href="/post/{{ $post->id }}" class="card-link">
+            <div class="card ml-2 mb-2 list">
+              <div style="min-height: 200px; width: 100%">
+                <img src="{{asset('storage/files/'. $post->filename)}}" class="img-fluid" />
+              </div>
+              <div class="card-body">
+                <h4 class="card-title text-dark h4 font-weight-bold">{{ $post->title }}</h4>
+                <p class="text-dark small">{{ $post->user->name }} | {{ $post->created_at }}</p>
+                <p class="text-dark text-truncate">{{ $post->description }}</p>
+                <div class="text-dark text-right">
+                  <i class="fas fa-thumbs-up ml-2 mr-1"></i> {{ count($post->likes) }}
+                </div>
               </div>
             </div>
-          </div>
-        </a>
-      </div>
+          </a>
+        </div>
+        @endforeach
       @endforeach
-    @endforeach
+    </div>
   </div>
 </div>
 @endsection
