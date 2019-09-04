ad859d6deb40516f873d5a8ecadb89d4d2ea003e
Micaela <eladelmonte@gmail.com>
2019-09-04 17:18:48 +0900

    UPDATED homepage design

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -14,23 +14,32 @@
 </div>
 @if(isset($posts))
 <div class="justify-content-center text-center">
-<h1>Featured Article</h1>
-    @foreach($posts as $item)
-    <div class="card" style="">
-    <div class="row" style="height: 200px; width: 100%; object-fit: cover; background-position: left; background: linear-gradient(to right, rgba(255, 255, 255, 0) 0, rgb(255, 255, 255) 70%, rgba(255, 255, 255, 1) 290px), url({{asset('storage/files/'. $item->filename)}}) center center no-repeat; background-size: cover;">
-        <!-- <div class="col-6">
-        <img src="{{asset('storage/files/'. $item->filename)}}" width="50%" class="featured-container">
-        </div> -->
-        <div class="col-md-6 offset-md-6">
-            <div class="card-body text-right">
-                <h4 class="card-title text-primary">{{ $item->title }}</h4>
-                <p>{{ $item->created_at }}</p>
-                <p class="card-text">{{ $item->description }}</p>
-            </div>
+<div class="my-5">
+  <span class="featured-title">
+    <span class="font-weight-bold">Featured</span><span class="font-weight-light">Articles</span>
+  </span>
+</div>
+@foreach($posts as $item)
+<div class="card" style="">
+<div class="row" style="height: 200px; width: 100%; object-fit: cover; background-position: left; background: linear-gradient(to right, rgba(255, 255, 255, 0) 0, rgb(255, 255, 255) 70%, rgba(255, 255, 255, 1) 290px), url({{asset('storage/files/'. $item->filename)}}) center center no-repeat; background-size: cover;">
+    <!-- <div class="col-6">
+    <img src="{{asset('storage/files/'. $item->filename)}}" width="50%" class="featured-container">
+    </div> -->
+    <div class="col-md-6 offset-md-6">
+        <div class="card-body text-right">
+            <h4 class="card-title text-primary">{{ $item->title }}</h4>
+            <p>{{ $item->created_at }}</p>
+            <p class="card-text">{{ $item->description }}</p>
         </div>
-    </div>   
     </div>
-    @endforeach
+</div>   
+</div>
+@endforeach
+<div class="my-5">
+  <span class="featured-title">
+    <span class="font-weight-bold">About</span><span class="font-weight-light">Us</span>
+  </span>
+</div>
 </div>
 
 @endif
