8793b3918272f6792c16c73ccdac57e768d60d7a
Micaela <eladelmonte@gmail.com>
2019-09-04 17:59:42 +0900

    UPDATED create & update ui design

--- resources/views/post_create.blade.php
+++ resources/views/post_create.blade.php
@@ -3,8 +3,10 @@
 @section('content')
 <div class="container">
   <div class="blog-content mt-5">
-	<div class="d-flex">
-		<h2 class="mt-3 mb-2">Add New Post</h2>
+	<div class="text-center">
+		<span class="mt-3 mb-2 blog-create__title">
+			Add New Post
+		</span>
 	</div>
 	@if (session('create_success'))
 	<div class="d-flex justify-content-center">
@@ -35,7 +37,7 @@
 		<div class="form-group blog-form">
 			<div class="row">
 			<div class="col-sm-4">
-				<label class="h5" for="url">Upload image or video</label>
+				<label class="blog-create__label" for="url">Upload image or video</label>
 				<input type="file" class="form-control-file" id="fileToUpload" name="fileToUpload">
 				<small id="fileHelp" class="form-text text-muted">Please upload a valid image file. Size of
 				image should
@@ -49,7 +51,7 @@
 				<h4>OR</h4>
 			</div>
 			<div class="col-sm-7">
-				<label class="h5" for="url">Or you can also add link from youtube</label>
+				<label class="blog-create__label" for="url">Or you can also add link from youtube</label>
 				<input type="text" class="form-control" id="link" name="link"
 					placeholder="Please enter url link here">
 				<small class="form-text text-muted">Please provide the embedded link. 	EX. https://www.youtube.com/embed/DRu7clboPP0</small>
@@ -60,7 +62,7 @@
 			</div>
 		</div>
 		<div class="form-group blog-form">
-			<label class="h5" for="url">Category</label><br/>
+			<label class="blog-create__label" for="url">Category</label><br/>
 			<div class="row justify-content-center">
 			@foreach ($categories as $data)
 			<div class="col-3">
@@ -71,14 +73,16 @@
 		</div>
 		<!-- Create the editor container -->
 		<div class="form-group blog-form">
-			<label class="h5" for="description">Description</label><br/>
+			<label class="blog-create__label" for="description">Description</label><br/>
 			<textarea class="form-control" id="description" name="description" maxlength="500"></textarea>
 			<small class="form-text text-muted">Write a short description about your post</small>
 			@error('description')
 			<span class="text-danger">{{ $message }}</span>
 			@enderror
 		</div>
-		<button class="btn btn-primary" type="submit">Publish</button>
+		<div class="text-center">
+			<button class="btn blog-create__publish" type="submit">Publish</button>
+		</div>
 		</form>
 	</div>
   </div>
